8b7dc942843f9eacf46cf4a7776c595eb0c4686e
rayllanderson <rayllanderson@gmail.com>
2020-12-22 02:08:33 -0300

    iniciando projeto com simples programa

---
+++ src/test/java/com/ray/treinamentojsf/AppTest.java
@@ -0,0 +1,20 @@
+package com.ray.treinamentojsf;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
